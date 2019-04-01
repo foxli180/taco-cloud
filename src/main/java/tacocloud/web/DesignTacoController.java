514343a1a48518ab61dd5d298fef686eba8f7f92
Fox@Home <jiaqingli@gmail.com>
2019-04-01 22:32:29 +0800

    Chapter 2 continue with tacocload

--- src/main/java/tacocloud/web/DesignTacoController.java
+++ src/main/java/tacocloud/web/DesignTacoController.java
@@ -4,7 +4,10 @@ package tacocloud.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacocloud.ingredient;
 import tacocloud.Taco;
@@ -41,6 +44,25 @@ public class DesignTacoController {
         model.addAttribute("design", new Taco());
         return "design";
     }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
+
+//    @PostMapping
+//    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+//        if (errors.hasErrors()) {
+//            return "design";
+//        }
+//
+//        // Save the taco design...
+//        // We'll do this in chapter 3
+//        log.info("Processing design: " + design);
+//
+//        return "redirect:/orders/current";
+//    }
 
     private List<ingredient> filterByType(
             List<ingredient> ingredients, ingredient.Type type) {
